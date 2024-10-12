86f2a94ece6422dffc12de8555080e6e031c7a5b
OneWay13 <96326903+OneWay13@users.noreply.github.com>
2024-10-12 09:18:50 +0700

    Add files via upload

---
+++ Project Komunitas/js/script.js
@@ -0,0 +1,70 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar navbar untuk menghilangkan menu
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// klik normal slide show tombol navigati or dot
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("imgslide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Automatic Slideshow
+let autoSlideIndex = 0;
+showSlidesAuto();
+
+function showSlidesAuto() {
+  let i;
+  let slides = document.getElementsByClassName("imgslide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  autoSlideIndex++;
+  if (autoSlideIndex > slides.length) {
+    autoSlideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[autoSlideIndex - 1].style.display = "block";
+  dots[autoSlideIndex - 1].className += " active";
+  setTimeout(showSlidesAuto, 5000); // Change image every 5 seconds
+}
